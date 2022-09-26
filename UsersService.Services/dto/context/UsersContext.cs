d80098fbc605f4aabd0a68b27942bf5660558a5b
artem-gck <g.artema31@gmail.com>
2022-09-26 19:38:07 +0300

    Add supporting SQL Server

---
+++ UsersService.Services/dto/context/UsersContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersService.Services.dto.context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<UserDto> Users { get; set; } = null!;
+        public DbSet<UserInfoDto> UsersInfo { get; set; } = null!;
+        public DbSet<RoleDto> Roles { get; set; } = null!;
+        public DbSet<TokensDto> Tokens { get; set; } = null!;
+
+        public UsersContext(DbContextOptions<UsersContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
