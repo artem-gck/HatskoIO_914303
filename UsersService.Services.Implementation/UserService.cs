eac95cacb64ad301e9d6ad4cf2c608acfe10f6d8
artem-gck <g.artema31@gmail.com>
2022-09-26 23:54:45 +0300

    Add service level interface

--- UsersService.Services.Implementation/UserService.cs
+++ UsersService.Services.Implementation/UserService.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsersService.Services.Models;
 
 namespace UsersService.Services.Implementation
 {
     public class UserService : IUserService
     {
-        
+        public int AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- UsersService.Services/IUserService.cs
+++ UsersService.Services/IUserService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsersService.Services.Models;
 
 namespace UsersService.Services
 {
     public interface IUserService
     {
+        public IEnumerable<User> GetUsers();
+        public User GetUser(int id);
+        public int DeleteUser(int id);
+        public int AddUser(User user);
+        public int UpdateUser(User user);
     }
 }
