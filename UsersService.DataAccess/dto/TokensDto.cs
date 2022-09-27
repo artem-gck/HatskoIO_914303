62a4e6e05bc8e3708d30f092da4c22cbcfad3e25
artem-gck <g.artema31@gmail.com>
2022-09-27 09:26:14 +0300

    Formating types of links between dto's

--- UsersService.DataAccess/dto/TokensDto.cs
+++ UsersService.DataAccess/dto/TokensDto.cs
@@ -9,8 +9,8 @@ namespace UsersService.DataAccess.dto
     public class TokensDto
     {
         public int Id { get; set; }
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
         public UserDto? User { get; set; }
     }
 }
--- UsersService.DataAccess/dto/UserDto.cs
+++ UsersService.DataAccess/dto/UserDto.cs
@@ -12,7 +12,7 @@ namespace UsersService.DataAccess.dto
         public string Login { get; set; }
         public string Password { get; set; }
         public RoleDto Role { get; set; }
-        public TokensDto? Tokens { get; set; }
+        public TokensDto Tokens { get; set; }
         public UserInfoDto UserInfo { get; set; }
     }
 }
