62b15ee82e196bc95ec058a47e42bdba5026d31f
artem-gck <g.artema31@gmail.com>
2022-09-28 22:26:31 +0300

    Initial commit for UserLoginService

---
+++ MicroServices/UserLoginService/UserLoginService.Domain/Entities/UserLoginEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace UserLoginService.Domain.Entities
+{
+    
+    public class UserLoginEntity
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public RoleEntity Role { get; set; }
+        public int? TockenId { get; set; }
+        public int? UserInfoId { get; set; }
+    }
+}
