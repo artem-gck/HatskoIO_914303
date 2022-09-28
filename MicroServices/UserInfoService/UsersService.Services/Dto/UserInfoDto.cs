145ec2007c19982ca8767de3a07c5aab367c7b1b
artem-gck <g.artema31@gmail.com>
2022-09-28 15:50:43 +0300

    Remove unnecessary namespaces

--- MicroServices/UserInfoService/UsersService.Services/Dto/UserInfoDto.cs
+++ MicroServices/UserInfoService/UsersService.Services/Dto/UserInfoDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UsersService.Services.Dto
+﻿namespace UsersService.Services.Dto
 {
     public class UserInfoDto
     {
