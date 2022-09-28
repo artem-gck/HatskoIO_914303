80be4b39798be3c4be556afc74a772176f041bdc
artem-gck <g.artema31@gmail.com>
2022-09-28 21:49:21 +0300

    Add swagger

--- MicroServices/UserInfoService/UsersService/Program.cs
+++ MicroServices/UserInfoService/UsersService/Program.cs
@@ -22,8 +22,15 @@ builder.Services.AddScoped<IUserAccess, UserAccess>();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
