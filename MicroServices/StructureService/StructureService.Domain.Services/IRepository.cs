3cdb2a13355ce6eae4e1439627f4814ce141da63
artem-gck <g.artema31@gmail.com>
2022-09-30 19:13:07 +0300

    Add realisation of Domain.Services

---
+++ MicroServices/StructureService/StructureService.Domain.Services/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using StructureService.Domain.Entities;
+
+namespace StructureService.Domain.Services
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public Task<T> GetAsync(int id);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<int> AddAsync(T entity);
+        public Task<int> DeleteAsync(int id);
+        public Task<int> UpdateAsync(int id, T entity);
+    }
+}
