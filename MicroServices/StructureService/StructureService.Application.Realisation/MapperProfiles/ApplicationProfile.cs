4e2755f7794b35abc79b5884e2b6b4c934b16bef
artem-gck <g.artema31@gmail.com>
2022-09-30 23:21:05 +0300

    Add Application.Realisation

---
+++ MicroServices/StructureService/StructureService.Application.Realisation/MapperProfiles/ApplicationProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using StructureService.Application.Services.Dto;
+using StructureService.Domain.Entities;
+
+namespace StructureService.Application.Realisation.MapperProfiles
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<DepartmentEntity, DepartmentDto>();
+            CreateMap<DepartmentDto, DepartmentEntity>();
+            CreateMap<DepartmentUnitEntity, DepartmentUnitDto>();
+            CreateMap<DepartmentUnitDto, DepartmentUnitEntity>();
+            CreateMap<PositionEntity, PositionDto>();
+            CreateMap<PositionDto, PositionEntity>();
+        }
+    }
+}
---
+++ MicroServices/StructureService/StructureService.Application.Realisation/Service.cs
@@ -0,0 +1,46 @@
+﻿using StructureService.Application.Services.Dto;
+using StructureService.Application.Services;
+using StructureService.Domain.Entities;
+using StructureService.Domain.Services;
+using AutoMapper;
+
+namespace StructureService.Application.Realisation
+{
+    public class Service<TDto, TEntity> : IService<TDto> where TDto : BaseDto
+                                                         where TEntity : BaseEntity
+    {
+        private readonly IRepository<TEntity> _repository;
+        private readonly IMapper _applicationMapper;
+
+        public Service(IRepository<TEntity> repository, IMapper applicationMapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _applicationMapper = applicationMapper ?? throw new ArgumentNullException(nameof(applicationMapper));
+        }
+
+        public async Task<int> AddAsync(TDto dto)
+        {
+            return await _repository.AddAsync(_applicationMapper.Map<TEntity>(dto));
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<TDto>> GetAllAsync()
+        {
+            return (await _repository.GetAllAsync()).Select(us => _applicationMapper.Map<TDto>(us));
+        }
+
+        public async Task<TDto> GetAsync(int id)
+        {
+            return _applicationMapper.Map<TDto>(await _repository.GetAsync(id));
+        }
+
+        public async Task<int> UpdateAsync(int id, TDto dto)
+        {
+            return await _repository.UpdateAsync(id, _applicationMapper.Map<TEntity>(dto));
+        }
+    }
+}
