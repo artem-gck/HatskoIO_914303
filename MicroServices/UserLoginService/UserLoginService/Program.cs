3d7f8130e67803dd5233f53e8304356a64b9f5f2
artem-gck <g.artema31@gmail.com>
2022-09-30 11:39:49 +0300

    Resolve conflict

--- MicroServices/UserLoginService/UserLoginService/Program.cs
+++ MicroServices/UserLoginService/UserLoginService/Program.cs
@@ -1,7 +1,35 @@
+<<<<<<< Updated upstream
+=======
+using UserLoginService.Application.Services;
+using UserLoginService.Application.Realisation;
+using UserLoginService.Application.Realisation.MapperProfiles;
+using UserLoginService.Domain.Entities;
+using UserLoginService.Domain.Realisation;
+using UserLoginService.Domain.Services;
+using UserLoginService.MapperProfiles;
+using Microsoft.EntityFrameworkCore;
+using UserLoginService.Domain.Realisation.Context;
+
+>>>>>>> Stashed changes
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("UserLoginConnection");
 
 // Add services to the container.
 
+<<<<<<< Updated upstream
+=======
+builder.Services.AddAutoMapper(typeof(ApplicationProfile), typeof(ControllerProfile));
+
+builder.Services.AddDbContext<UserLoginContext>(opt =>
+    opt.UseSqlServer(connectionString, b => b.MigrationsAssembly("UserLoginService.Domain.Realisation")));
+
+builder.Services.AddScoped<IRepository<UserLoginEntity>, UserLoginRepository>();
+builder.Services.AddScoped<IRepository<RoleEntity>, RoleRopository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+
+>>>>>>> Stashed changes
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
