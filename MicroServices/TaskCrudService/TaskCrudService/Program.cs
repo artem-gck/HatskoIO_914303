89f04baea39c6131c44db714b50c6bf28eb23ca7
artem-gck <g.artema31@gmail.com>
2022-10-02 18:08:11 +0300

    Add migration

--- MicroServices/TaskCrudService/TaskCrudService/Program.cs
+++ MicroServices/TaskCrudService/TaskCrudService/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TaskCrudService.Domain.Realisation.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TaskConnection");
 
 // Add services to the container.
+
+builder.Services.AddDbContext<TaskContext>(opt =>
+    opt.UseSqlServer(connectionString, b => b.MigrationsAssembly("TaskCrudService.Domain.Realisation")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
