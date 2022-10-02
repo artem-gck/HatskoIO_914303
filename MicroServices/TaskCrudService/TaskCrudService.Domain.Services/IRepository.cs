f548297dc48547026654848a3ec01c366a25baeb
artem-gck <g.artema31@gmail.com>
2022-10-02 15:47:58 +0300

    Add domain.services

---
+++ MicroServices/TaskCrudService/TaskCrudService.Domain.Services/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskCrudService.Domain.Entities;
+
+namespace TaskCrudService.Domain.Services
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public Task<T> GetAsync(Guid id);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<Guid> AddAsync(T entity);
+        public Task<Guid> DeleteAsync(Guid id);
+        public Task<Guid> UpdateAsync(Guid id, T entity);
+    }
+}
