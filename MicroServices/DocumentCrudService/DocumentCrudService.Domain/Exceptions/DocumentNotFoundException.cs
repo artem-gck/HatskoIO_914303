54466f8186930ee64c1d52cb492302a7dd79ed95
artem-gck <g.artema31@gmail.com>
2022-10-03 11:04:10 +0300

    Add domain

---
+++ MicroServices/DocumentCrudService/DocumentCrudService.Domain/Exceptions/DocumentNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace DocumentCrudService.Domain.Exceptions
+{
+    public class DocumentNotFoundException : Exception
+    {
+        public DocumentNotFoundException(string name)
+            : base($"Document with {name} not found")
+        {
+
+        }
+    }
+}
