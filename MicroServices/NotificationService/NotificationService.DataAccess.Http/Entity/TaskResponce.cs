72946c3df75a2b89ae20f3b8b4f3df68f2c207ce
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-10-18 13:38:18 +0300

    Add repository layer

---
+++ MicroServices/NotificationService/NotificationService.DataAccess.Http/Entity/TaskResponce.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.DataAccess.Http.Entity
+{
+    public class TaskResponce
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Header { get; set; }
+        public Guid OwnerUserId { get; set; }
+        public DateTime DeadLine { get; set; }
+        public List<ArgumentResponce>? Arguments { get; set; }
+    }
+}
