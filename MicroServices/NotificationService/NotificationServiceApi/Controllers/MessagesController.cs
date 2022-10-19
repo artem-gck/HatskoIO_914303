8689a9331246f8d24471a98cb4efa42bf756cca2
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-10-19 15:06:58 +0300

    Add exception handler middleware

--- MicroServices/NotificationService/NotificationServiceApi/Controllers/MessagesController.cs
+++ MicroServices/NotificationService/NotificationServiceApi/Controllers/MessagesController.cs
@@ -30,7 +30,11 @@ namespace NotificationServiceApi.Controllers
         ///     GET /api/messages?page=1 count=10
         ///
         /// </remarks>
+        /// <response code="200">Return models</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IEnumerable<MessageResponce>> Get(int page = 1, int count = 10)
             => _mapper.Map<IEnumerable<MessageResponce>>(await _messageService.GetAsync(page, count));
 
@@ -45,7 +49,13 @@ namespace NotificationServiceApi.Controllers
         ///     GET /api/messages/{id}
         ///
         /// </remarks>
+        /// <response code="200">Return model</response>
+        /// <response code="404">Message not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<MessageResponce> Get(Guid id)
             => _mapper.Map<MessageResponce>(await _messageService.GetAsync(id));
     }
---
+++ MicroServices/NotificationService/NotificationServiceApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using NotificationService.DataAccess.DataBase.Exceptions;
+using NotificationService.DataAccess.Http.Exceptions;
+using System.Net;
+
+namespace NotificationServiceApi.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                var response = httpContext.Response;
+
+                response.StatusCode = ex switch
+                {
+                    NotFoundMessageException    => (int)HttpStatusCode.NotFound,
+                    NotFoundException           => (int)HttpStatusCode.NotFound,
+                    Exception                   => (int)HttpStatusCode.InternalServerError,
+                };
+
+                await response.WriteAsJsonAsync(new { message = ex?.Message });
+            }
+        }
+    }
+}
