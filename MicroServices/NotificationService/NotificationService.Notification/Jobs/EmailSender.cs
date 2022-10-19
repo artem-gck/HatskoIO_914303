7073f1dcbc39e56c42c0c0525657da9c7a7f6aab
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-10-19 11:18:22 +0300

    Add adding message to db from job

--- MicroServices/NotificationService/NotificationService.Notification/Jobs/EmailSender.cs
+++ MicroServices/NotificationService/NotificationService.Notification/Jobs/EmailSender.cs
@@ -3,6 +3,8 @@ using System.Net.Mail;
 using System.Net;
 using NotificationService.DataAccess.Http.Interfaces;
 using Microsoft.Extensions.Configuration;
+using NotificationService.DataAccess.DataBase;
+using NotificationService.DataAccess.DataBase.Entity;
 
 namespace NotificationService.Notification.Jobs
 {
@@ -13,12 +15,14 @@ namespace NotificationService.Notification.Jobs
         private readonly string _host;
 
         private readonly ITaskAccess _taskRepository;
-        private readonly IManagementAccess _managementRepository;
+        private readonly IManagementAccess _managementAccess;
+        private readonly IMessageRepository _messageRepository;
 
-        public EmailSender(ITaskAccess taskRepository, IManagementAccess managementRepository, IConfiguration configuration)
+        public EmailSender(ITaskAccess taskRepository, IManagementAccess managementAccess, IMessageRepository messageRepository, IConfiguration configuration)
         {
             _taskRepository = taskRepository ?? throw new ArgumentNullException(nameof(taskRepository));
-            _managementRepository = managementRepository ?? throw new ArgumentNullException(nameof(managementRepository));
+            _managementAccess = managementAccess ?? throw new ArgumentNullException(nameof(managementAccess));
+            _messageRepository = messageRepository ?? throw new ArgumentNullException(nameof(messageRepository));
 
             _senderEmail = configuration["EmailSettings:Email"];
             _senderPassword = configuration["EmailSettings:Password"];
@@ -28,11 +32,11 @@ namespace NotificationService.Notification.Jobs
         public async Task Execute(IJobExecutionContext context)
         {
             var tasks = (await _taskRepository.GetTasksAsync())
-                            .Where(task => (task.DeadLine - DateTime.UtcNow) <= TimeSpan.FromDays(1));
+                            .Where(task => (task.DeadLine - DateTime.Now) <= TimeSpan.FromDays(1));
 
             foreach (var task in tasks)
             {
-                var user = await _managementRepository.GetUserInfoAsync(task.OwnerUserId);
+                var user = await _managementAccess.GetUserInfoAsync(task.OwnerUserId);
 
                 using var message = new MailMessage(_senderEmail, user.Email);
 
@@ -48,6 +52,15 @@ namespace NotificationService.Notification.Jobs
                 };
 
                 await smtpClient.SendMailAsync(message);
+
+                var messageEntity = new MessageEntity
+                {
+                    RecipientId = user.Id,
+                    Subject = message.Subject,
+                    Body = message.Body,
+                };
+
+                await _messageRepository.AddAsync(messageEntity);
             }
         }
     }
