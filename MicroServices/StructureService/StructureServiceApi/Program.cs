62b9daba0b915c165b120b6fa23546748c4054e4
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-10-29 15:38:13 +0300

    Add connection strings to appsettings

--- MicroServices/StructureService/StructureServiceApi/Program.cs
+++ MicroServices/StructureService/StructureServiceApi/Program.cs
@@ -20,9 +20,9 @@ using Messages;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("StructureConnection");
 
-var connectionStringAzure = "Endpoint=sb://document-flow.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+xItHOUEoMT6c9//vrNA93XfJQmOAZykczU7zkfeXKI=";
-var newPurchaseTopic = "new-user-topic";
-var subscriptionName = "structure-service";
+var connectionStringAzure = builder.Configuration.GetConnectionString("ServiceBus");
+var newPurchaseTopic = builder.Configuration["Topics:NewUser"];
+var subscriptionName = builder.Configuration["SubscriptionName"];
 
 // Add services to the container.
 
