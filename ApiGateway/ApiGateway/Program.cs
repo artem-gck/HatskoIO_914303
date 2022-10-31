9643fb1b728c77a71bdc9c67980e59ae5ed12854
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-10-31 14:34:02 +0300

    Add structure of api

---
+++ ApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
